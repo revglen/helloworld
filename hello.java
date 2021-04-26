649b23482e2919ed09401c56647bb335c78d752b
Glen Saldanha <revglen@gmail.com>
2021-04-26 19:11:26 +0100

    added new code

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
 public class hello{
   public static void main(String[] args){
-    for(int i=0; i < 10; i++)
+    
+    System.out.println("About to start iteration...");
+    for(int i=0; i < 10; i++){
       System.out.println("The number is " + (i+1));
+    }
+    System.out.println("Iteration completed...");
   }
 }
